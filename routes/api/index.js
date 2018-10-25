955f7c54a3d85d2435c161f92aae4ebe51819157
nosrednaski <nosrednaski@gmail.com>
2018-10-25 12:26:14 -0400

    deploying to heroku changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
-const articleRoutes = require("./articles");
+const articleRoutes = require("./articleRoutes");
 
-// Book routes
 router.use("/articles", articleRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// require db connection
+require('./models');
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,8 +17,6 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/NYT");
 
 // Start the API server
 app.listen(PORT, function() {
